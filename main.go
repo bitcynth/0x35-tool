6334724c4ae9e2aa650495c4f0166d7d269c5476
Cynthia Revström <me@cynthia.re>
2020-05-27 00:14:25 +0200

    Basic cli working

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 var configFile = flag.String("config", "$HOME/.0x35-tool.json", "the path to the config file")
+var syncFlag = flag.Bool("sync", false, "update the 0x35 servers with the zone file(s) provided")
 
 func main() {
 	flag.Parse()
+
+	loadConfig()
+
+	if *syncFlag {
+		// In the case of sync, all non-flag args are zone files
+		for _, zoneFile := range flag.Args() {
+			fmt.Printf("Updating from file: %s\n", zoneFile)
+		}
+	}
 }
