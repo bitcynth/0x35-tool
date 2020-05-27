f15bb85fda5c51a6a3210608a23decd6362eb116
Cynthia Revström <me@cynthia.re>
2020-05-27 15:31:10 +0200

    Parse the zone file, fetch the zone, and list the changes in main.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var configFile = flag.String("config", "$HOME/.0x35-tool.json", "the path to the config file")
@@ -17,6 +18,31 @@ func main() {
 		// In the case of sync, all non-flag args are zone files
 		for _, zoneFile := range flag.Args() {
 			fmt.Printf("Updating from file: %s\n", zoneFile)
+
+			header, entries, err := parseZoneFile(zoneFile)
+			if err != nil {
+				fmt.Printf("failed to parse file: %v\n", err)
+				os.Exit(1)
+			}
+
+			// Fetch the existing zone from PDNS
+			pdnsZone, err := pdnsGetZone(header.Name)
+			if err == errNoSuchZone {
+				fmt.Printf("no such zone, treating as new zone\n")
+			} else if err != nil {
+				fmt.Printf("failed to get zone from server: %v\n", err)
+			}
+
+			// List the changes
+			rrSetChanges, err := pdnsGetZoneChanges(pdnsZone, header, entries)
+			if err == errAlreadyUpToDate {
+				fmt.Printf("Zone already up to date!\n")
+				continue
+			}
+			changes := formatChangesList(rrSetChanges, pdnsZone)
+			for _, change := range changes {
+				fmt.Printf("%s\n", change)
+			}
 		}
 	}
 }
