073d867cbfbbc28ed0c65eba3934938457c3ebc2
Cynthia Revström <me@cynthia.re>
2020-05-27 15:44:47 +0200

    Finish the initial CLI for updates!

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var configFile = flag.String("config", "$HOME/.0x35-tool.json", "the path to the config file")
@@ -27,7 +29,9 @@ func main() {
 
 			// Fetch the existing zone from PDNS
 			pdnsZone, err := pdnsGetZone(header.Name)
+			isNewZone := false
 			if err == errNoSuchZone {
+				isNewZone = true
 				fmt.Printf("no such zone, treating as new zone\n")
 			} else if err != nil {
 				fmt.Printf("failed to get zone from server: %v\n", err)
@@ -43,6 +47,25 @@ func main() {
 			for _, change := range changes {
 				fmt.Printf("%s\n", change)
 			}
+			fmt.Println()
+
+			// Ask if the user wants to proceed
+			cliReader := bufio.NewReader(os.Stdin)
+			fmt.Print("Do you want to update (type \"yes\" and hit enter): ")
+			text, _ := cliReader.ReadString('\n')
+			text = strings.ReplaceAll(text, "\r", "")
+			if text != "yes\n" {
+				continue
+			}
+
+			// Actually update the zone
+			err = pdnsUpdateZone(rrSetChanges, isNewZone, header)
+			if err != nil {
+				fmt.Printf("failed to update zone: %v\n", err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("Zone (%s) updated!\n", header.Name)
 		}
 	}
 }
