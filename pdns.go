02e735de9d60ef981b620e8590f32478da4613c7
Cynthia Revström <me@cynthia.re>
2020-05-27 15:19:40 +0200

    Add zone change detection logic
    This adds a function to list the changes that would be made to the zone.

--- pdns.go
+++ pdns.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // Represents a zone to be passed to the PowerDNS API
@@ -39,6 +40,7 @@ type pdnsComment struct {
 }
 
 var errNoSuchZone = errors.New("the zone doesn't exist")
+var errAlreadyUpToDate = errors.New("the zone is already up to date")
 
 func pdnsCallAPI(method string, url string, payload interface{}) (*http.Response, error) {
 	var body io.Reader
@@ -91,4 +93,138 @@ func pdnsGetZone(zoneName string) (pdnsZone, error) {
 	}
 
 	return zone, nil
+}
+
+// This method checks for changes that need to be made to the server zone.
+// This function is quite massive and could probably be a lot cleaner.
+func pdnsGetZoneChanges(initialZone pdnsZone, header zoneFileHeader, entries []zoneFileEntry) (map[string]pdnsRRSet, error) {
+	// Check if this is an update or creation
+	isNewZone := false
+	if initialZone.Name == "" {
+		isNewZone = true
+	}
+
+	url := fmt.Sprintf("%s/api/v1/servers/%s/zones", config.BaseURL, config.ServerID)
+	if !isNewZone {
+		url = fmt.Sprintf("%s/%s", url, header.Name)
+	}
+
+	// Entries are grouped by record type and name, example: "@-ALIAS"
+	rrSets := make(map[string]pdnsRRSet)
+	for _, entry := range entries {
+		rrSetName := entry.Name + "-" + entry.Type
+		ttl := entry.TTL
+
+		// if TTL is unspecified (-1), use the header TTL value
+		if ttl == -1 {
+			ttl = header.TTL
+		}
+
+		rrSet, ok := rrSets[rrSetName]
+		// Check if the RRSet (group of entries) already exists in the map
+		// and if not, create it.
+		if !ok {
+			rrSet = pdnsRRSet{
+				Name: entry.Name,
+				Type: entry.Type,
+				TTL:  ttl,
+			}
+		}
+
+		// Add the PNS record to the RRSet
+		record := pdnsRecord{
+			Content: entry.Data,
+		}
+		rrSet.Records = append(rrSet.Records, record)
+		rrSets[rrSetName] = rrSet
+	}
+
+	// Check that there is a single SOA record in the zone
+	if len(rrSets[header.Name+"-SOA"].Records) != 1 {
+		return nil, fmt.Errorf("you need exactly 1 SOA record")
+	}
+
+	// Check if SERIAL_INC is being used and store it.
+	// This is a bit of a hack to avoid serial changes being detected as a change.
+	soaContent := rrSets[header.Name+"-SOA"].Records[0].Content
+	usingSerialInc := false
+	rrSets[header.Name+"-SOA"].Records[0].Content = regexZoneVariable.ReplaceAllStringFunc(soaContent, func(in string) string {
+		if in[2:len(in)-2] == "SERIAL_INC" {
+			usingSerialInc = true
+			return strconv.Itoa(initialZone.Serial)
+		}
+		return in
+	})
+
+	rrSetChanges := make(map[string]pdnsRRSet)
+	rrSetInitialValues := make(map[string]pdnsRRSet)
+
+	// Loop through the initial RRSets and check what needs to be changed.
+	for _, rrSet := range initialZone.RRSets {
+		rrSetGroup := rrSet.Name + "-" + rrSet.Type
+		rrSetInitialValues[rrSetGroup] = rrSet // Store the initial state
+
+		// Check if the RRSet exists in the new state.
+		// If it doesn't exist, set change type to delete.
+		rrSetChange, ok := rrSets[rrSetGroup]
+		if !ok {
+			rrSetChange = rrSet
+			rrSetChange.ChangeType = "DELETE"
+			rrSetChanges[rrSetGroup] = rrSetChange
+		}
+
+		if rrSet.TTL != rrSetChange.TTL || len(rrSet.Records) != len(rrSetChange.Records) {
+			rrSetChange.ChangeType = "REPLACE"
+		}
+
+		for _, record := range rrSet.Records {
+			for i, changedRecord := range rrSetChange.Records {
+				if record == changedRecord {
+					// We found a record in the new state that matches the old state
+					break
+				}
+
+				if len(rrSetChange.Records) == i+1 {
+					rrSetChange.ChangeType = "REPLACE"
+				}
+			}
+		}
+
+		rrSetChanges[rrSetGroup] = rrSetChange
+	}
+
+	// Loop through new state and check the change type in the changes.
+	for _, rrSet := range rrSets {
+		rrSetGroup := rrSet.Name + "-" + rrSet.Type
+		rrSetChange, ok := rrSetChanges[rrSetGroup]
+		if ok {
+			if rrSetChange.ChangeType == "" {
+				// If the RRSet is already up to date, delete the change.
+				delete(rrSetChanges, rrSetGroup)
+			}
+			continue
+		}
+		rrSet.ChangeType = "ADD" // This is a temporary status for this tool, will be changed to REPLACE later.
+		rrSetChanges[rrSetGroup] = rrSet
+	}
+
+	// If there are no changes, return
+	if len(rrSetChanges) == 0 {
+		return nil, errAlreadyUpToDate
+	}
+
+	// Update the SERIAL_INC value and update the SOA record
+	if usingSerialInc {
+		soaRRSet := rrSets[header.Name+"-SOA"]
+		rrSets[header.Name+"-SOA"].Records[0].Content = regexZoneVariable.ReplaceAllStringFunc(soaContent, func(in string) string {
+			if in[2:len(in)-2] == "SERIAL_INC" {
+				return strconv.Itoa(initialZone.Serial + 1)
+			}
+			return in
+		})
+		soaRRSet.ChangeType = "REPLACE"
+		rrSetChanges[header.Name+"-SOA"] = soaRRSet
+	}
+
+	return rrSetChanges, nil
 }
